aff7f1e5eb46ebafe14767eb104d12a933d54e93
Lukas Peltner <heypelzi@gmx.de>
2024-01-18 15:20:07 +0100

    refactored code to use new Arzneimittel struct

--- src/main.rs
+++ src/main.rs
@@ -26,22 +26,43 @@ fn check_price(args: &Vec<String>) -> f64 {
     }
 }
 
+struct Arzneimittel {
+    total_price: f64,
+}
+
+impl Arzneimittel {
+    pub fn new(total_price: f64) -> Self {
+        Self {
+            total_price,
+        }
+    }
+    pub fn tax(&self) -> f64 {
+        self.total_price * 0.19 / 1.19
+    }
+    fn pharmacy_buying_price(&self) -> f64 {
+        (self.total_price - self.tax() - 0.2 - 0.21 - 8.35) / 1.03
+    }
+    pub fn pharmacy_profit(&self) -> f64 {
+        self.total_price - self.tax() - 0.2 - 0.21 - self.pharmacy_buying_price()
+    }
+    pub fn supplier_profit(&self) -> f64 {
+        self.pharmacy_buying_price() - self.producer_price()
+    }
+    pub fn producer_price(&self) -> f64 {
+        (self.pharmacy_buying_price() - 0.73) / 1.0315
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let price: f64 = check_price(&args);
-    let mwst = price - price / 1.19;
-    let aep = (price - mwst - 0.2 - 0.21 - 8.35) / 1.03;
-    let apo = price - mwst - 0.2 - 0.21 - aep;
-    let apu = (aep - 0.73) / 1.0315;
-    let gh = aep - apu;
+    let am = Arzneimittel::new(check_price(&args));
     if args.contains(&String::from("-p")) {
-        println!("{apo:.2}");
+        println!("{:.2}", am.pharmacy_profit());
     } else {
-    println!(
-        "Total price: {price:>8.2}€\n\
-        State Tax:   {mwst:>8.2}€\n\
-        Pharmacy:    {apo:>8.2}€\n\
-        Supplier:    {gh:>8.2}€\n\
-        Producer:    {apu:>8.2}€");
+    println!("Total price: {:>8.2}€", am.total_price);
+    println!("State Tax:   {:>8.2}€", am.tax());
+    println!("Pharmacy:    {:>8.2}€", am.pharmacy_profit());
+    println!("Supplier:    {:>8.2}€", am.supplier_profit());
+    println!("Producer:    {:>8.2}€", am.producer_price());
     }
 }
